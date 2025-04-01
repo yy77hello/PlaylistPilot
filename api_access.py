3a9c0207c4283f4d9419cf840f650067de0bfd76
Maxym <108109659+yy77hello@users.noreply.github.com>
2025-04-01 12:10:05 -0400

    Update api_access.py

--- api_access.py
+++ api_access.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+import pylast
 
 load_dotenv()  # loads from .env into this dotenv
 
@@ -10,3 +11,28 @@ LASTFM_API_SECRET = os.getenv("LASTFM_API_SECRET")
 
 print(LASTFM_API_KEY)
 print(LASTFM_API_SECRET)
+
+# Creating a variable that stores our connection to last.f,
+network = pylast.LastFMNetwork(
+    api_key=LASTFM_API_KEY,
+    api_secret=LASTFM_API_SECRET)
+
+"""
+Searches for a track on Last.fm.
+Inputs: Artist and track name
+Returns: A pylast.Track object if found, none otherwise.
+"""
+def get_lastfm_track(artist, track_name):
+    track = network.get_track(artist, track_name)
+    return track
+
+"""
+Gets tags for a track on Last.fm.
+Inputs: Track object 
+Returns: A list of Tag objects
+"""
+def get_track_tags(track):
+    if track:
+        tags = track.get_top_tags()
+        return tags
+    return [] # Only goes here if no valid track
