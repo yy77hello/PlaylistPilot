91d8d04cb8facd708a028c5d2f765d4071258925
Evan Magee <evan.m.magoo@gmail.com>
2025-05-04 18:38:04 -0400

    Tag Tracking, Get Top Tags function
    - Added a dictionary for tracking the amount of times a tag is used.
    - Added function to get the top tags from the graph.

--- TrackGraph.py
+++ TrackGraph.py
@@ -1,7 +1,11 @@
+from collections import defaultdict
+
 class TrackGraph:
     def __init__(self):
         # Initialize an empty graph. This one will be like a "meta graph" that holds all our songs
+        # Also initialize a dictionary for keeping track of how many times a tag is used.
         self.graph = {}
+        self.tag_usage = defaultdict(int)
 
     def add_node(self, node):
         """Add a node to the graph (song, artist, tag, etc.)."""
@@ -22,6 +26,8 @@ class TrackGraph:
         # Create relationship if we dont have it yet
         if relationship not in self.graph[node1]:
             self.graph[node1][relationship] = set()
+
+        is_new = node2 not in self.graph[node1][relationship]
         self.graph[node1][relationship].add(node2)
 
         # Add the reverse connection from node2 to node1 directly
@@ -31,16 +37,22 @@ class TrackGraph:
             if reverse_relationship not in self.graph[node2]: # so we don't add a duplicate
                 self.graph[node2][reverse_relationship] = set()
             self.graph[node2][reverse_relationship].add(node1)
+
+            if is_new:
+                self.tag_usage[node2] += 1
+
         elif relationship == "is_tag_of":
             reverse_relationship = "has_tag"
             if reverse_relationship not in self.graph[node2]: # so we don't add a duplicate
                 self.graph[node2][reverse_relationship] = set()
             self.graph[node2][reverse_relationship].add(node1)
+
         elif relationship == "is_by_artist":
             reverse_relationship = "has_track"
             if reverse_relationship not in self.graph[node2]: # so we don't add a duplicate
                 self.graph[node2][reverse_relationship] = set()
             self.graph[node2][reverse_relationship].add(node1)
+
         elif relationship == "has_track":
             reverse_relationship = "is_by_artist"
             if reverse_relationship not in self.graph[node2]: # so we don't add a duplicate
@@ -62,6 +74,7 @@ class TrackGraph:
             if relationship in node_data: # Inside node data we have keys (our relationships) and the values are dictionaries of the items we are reffering to
                 connected_nodes = node_data[relationship]  # Get the connected nodes for this relationship
                 return connected_nodes  # Return the set of connected nodes
+            return None
         else:
             # If the node doesn't exist in the graph or if the relationship doesnt exist, return an empty set
             return set()
@@ -81,3 +94,13 @@ class TrackGraph:
             # If the node doesn't exist in the graph we just return an empty list
             return []
 
+    '''
+    Returns List of Top Tags, Amount in Graph
+    Input: (optional) Top Amount to Return
+    Returns: Returns list of top tags in form [Tag, Amount Used]
+    '''
+    def get_top_tags(self, top=10):
+
+        print(self.tag_usage.items())
+        sorted_tags = sorted(self.tag_usage.items(), key=lambda x: x[1], reverse=True)
+        return sorted_tags[:top]
